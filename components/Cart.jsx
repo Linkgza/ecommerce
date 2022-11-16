7479670f1e1652505c5b35fd0fa20cf5392a89ca
Linkgza <109047450+Linkgza@users.noreply.github.com>
2022-11-16 17:39:25 -0600

    Success

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -130,7 +130,7 @@ const Cart = () => {
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
-                Pay with Stripe
+                Pay & Checkout
               </button>
             </div>
           </div>
